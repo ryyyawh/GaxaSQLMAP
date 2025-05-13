af459a165256aee6dae5b35327ee882fa5702be5
Xylays <aulidanraja@gmail.com>
2025-05-13 12:40:52 +0700

    Update nsmscan.py

--- nsmscan.py
+++ nsmscan.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-# NoSQLMap Copyright 2012-2017 NoSQLMap Development team
-# See the file 'doc/COPYING' for copying permission
-
+# © Reyy Is Dev
 
 from exception import GaxaSQLMAPException
 import ipcalc
